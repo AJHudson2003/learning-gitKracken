a021d54722e15199994c9909b3673e7d37f46ddc
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2019-11-12 09:35:55 -0500

    ADD HelloWorld Function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
 public class Main {
+    public static void helloWorld(){
+        System.out.println("Hello world");
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        helloWorld();
+
     }
 }
