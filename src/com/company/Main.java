c98a1fabf889fc6803f13dd1f0544bdcf225f2b0
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2019-11-12 09:53:16 -0500

    ADD weather function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,12 @@ public class Main {
         System.out.println("Hello world");
     }
 
+    public static void weather(){
+        System.out.println("it is snowing");
+    }
     public static void main(String[] args) {
         helloWorld();
+        weather();
 
     }
 }
